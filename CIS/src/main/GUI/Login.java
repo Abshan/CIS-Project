ce8e5d17a8952407c0ccf5c03df0118edbea3400
Mohamed Abshan <40865976+Abshan@users.noreply.github.com>
2019-05-16 02:15:55 +0530

    Touch Inputs

--- CIS/src/main/GUI/Login.java
+++ CIS/src/main/GUI/Login.java
@@ -10,6 +10,8 @@ import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.rmi.Naming;
+
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -18,8 +20,8 @@ import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.border.LineBorder;
+
 import main.Servers.serverInterface;
-import javax.swing.ImageIcon;
 
 public class Login {
 
--- CIS/src/main/Servers/DBConnect.java
+++ CIS/src/main/Servers/DBConnect.java
@@ -9,6 +9,11 @@ import java.sql.DriverManager;
 
 public class DBConnect {
 	public Connection getConnection() {
+		
+		/*
+		 * Referenced Material: https://docs.oracle.com/javase/tutorial/jdbc/basics/connecting.html
+		 */
+		
 		String username = "root";
 		String password = "root";
 
