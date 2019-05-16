426acd2018962714164394e434d9ce15cb8bcbdb
Mohamed Abshan <40865976+Abshan@users.noreply.github.com>
2019-05-16 13:40:27 +0530

    Update Server.java

--- CIS/src/main/Servers/Server.java
+++ CIS/src/main/Servers/Server.java
@@ -5,12 +5,14 @@ package main.Servers;
  */
 
 import java.rmi.*;
+import java.rmi.registry.LocateRegistry;
 
 public class Server {
 	public static void main(String[] argv) {
 		try {
-
-			Naming.rebind("rmi://192.168.43.123/test", new functions());
+			
+			LocateRegistry.createRegistry(1099);
+			Naming.rebind("rmi://192.168.43.123:1099/test", new functions());
 
 			System.out.println("Server is ready");
 
